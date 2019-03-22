a69f8e6eda97b0fcad70c4d46e05fafb96f49514
zstegrios <zstergios@gmail.com>
2019-03-22 18:07:04 +0200

    Optimizes Code v1.1
    Add Session Caching

--- system/library/cache.php
+++ system/library/cache.php
@@ -1,62 +1,125 @@
 <?php
-class Cache {
+/**
+ * @package		Opencart v1.5 Advanced Caching
+ * @version     1.1
+ * @author      Stergios Zgouletas <info@web-expert.gr>
+ * @link        http://www.web-expert.gr
+ * @copyright   Copyright (C) 2010 Web-Expert.gr All Rights Reserved
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+**/
+
+class Cache
+{
 	private $expire = 3600;
-	private $apcEnabled=false;
+	private $APCmode=false;
 	
 	public function __construct($expire=3600)
 	{
+		if(!isset($_SESSION['opencartCache'])) $_SESSION['opencartCache']=array();
+		
 		$this->expire = $expire;
-		$this->apcEnabled=extension_loaded('apc');
+		$this->APCmode=false;
+		
+		if(extension_loaded('apcu')) //APC new version
+			$this->APCmode='apcu';
+		elseif (extension_loaded('apc')) //APC old version
+			$this->APCmode='apc';
+	}
+	
+	public function __destruct()
+	{
+		unset($_SESSION['opencartCache']); //clear session on close, prevent storing
 	}
 
 	public function get($key)
-	{
-		if($this->apcEnabled) return apc_fetch(HTTP_SERVER.$key);
-	
-		$files = glob(DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', $key) . '.*');
-
-		if ($files)
+	{	
+		//APC(u)
+		if($this->APCmode!==false)
 		{
-			$cache = file_get_contents($files[0]);
-			$data = unserialize($cache);
-			foreach ($files as $file)
-			{
-				$time = substr(strrchr($file, '.'), 1);
-				if ($time < time() && file_exists($file))
-				{
-					unlink($file);
-				}
-			}
-			return $data;
+			return $this->APCmode=='apcu'?apcu_fetch(HTTP_SERVER.$key):apc_fetch(HTTP_SERVER.$key);
 		}
+		
+		$hashKey=md5(HTTP_SERVER.$key);
+		
+		//Session Cache
+		$sessCache=$this->sessionCache($hashKey);
+		if($sessCache!==false && isset($sessCache['time']) && $sessCache['time']+$this->expire>=time()) return $sessCache['data'];
+		
+		//File Cache
+		$file=DIR_CACHE. $hashKey.'.cache';
+		if(file_exists($file))
+		{
+			$cache = file_get_contents($file);
+			$data = unserialize($cache);
+			if($data!==false && isset($data['time']) && $data['time']+$this->expire>=time())
+			{
+				return $data['data'];
+			}
+			$this->delete($key); //expired;
+		}
+		return false;
 	}
 
 	public function set($key, $value)
 	{
-		if($this->apcEnabled) return apc_store(HTTP_SERVER . $key, $value, $this->expire);
+		//APC(u)
+		if($this->APCmode!==false)
+		{
+			$this->delete($key);
+			return $this->APCmode=='apcu'? apcu_add(HTTP_SERVER . $key, $value, $this->expire) : apc_store(HTTP_SERVER . $key, $value, $this->expire);
+		}
 		
-		$this->delete($key);
-		$file = DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', $key) . '.' . (time() + $this->expire);
-		$handle = fopen($file, 'w');
-		fwrite($handle, serialize($value));
-		fclose($handle);
+		$hashKey=md5(HTTP_SERVER.$key);
+		$file=DIR_CACHE.$hashKey.'.cache';
+		
+		$cache=array(
+			'time'=>time(),
+			'data'=>$value,
+		);
+		
+		//Session Cache
+		$this->sessionCache('write',$hashKey,$cache);
+		//File Cache
+		file_put_contents($file,serialize($cache),LOCK_EX);
 	}
 
 	public function delete($key)
 	{
-		if($this->apcEnabled) return apc_delete(HTTP_SERVER.$key);
-		
-		$files = glob(DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', $key) . '.*');
-
-		if ($files)
+		//APC(u)
+		if($this->APCmode!==false)
 		{
-			foreach ($files as $file)
-			{
-				if (file_exists($file))
-				{
-					unlink($file);
-				}
-			}
+			return $this->APCmode=='apcu'? apcu_delete(HTTP_SERVER.$key) : apc_delete(HTTP_SERVER.$key);
 		}
+		
+		$hashKey=md5(HTTP_SERVER.$key);
+		$file=DIR_CACHE.$hashKey.'.cache';
+		
+		
+		//Session Cache
+		$this->sessionCache('delete',$hashKey);
+		
+		//File Cache
+		if(file_exists($file))
+		{
+			unlink($file);
+		}
+	}
+	
+	//session cache prevents re-opening the file when multiple files asking for specific key
+	protected function sessionCache($mode,$cacheKey,$newData=NULL)
+	{
+		if(isset($_SESSION['opencartCache'][$cacheKey]) && $mode=='delete')
+		{
+			unset($_SESSION['opencartCache'][$cacheKey]);
+		}
+		elseif($mode=='write')
+		{
+			$_SESSION['opencartCache'][$cacheKey]=serialize($newData);
+		}
+		else
+		{
+			return isset($_SESSION['opencartCache'][$cacheKey])? unserialize($_SESSION['opencartCache'][$cacheKey]) :false;
+		}
+		return false;
 	}
 }
